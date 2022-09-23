2d71f22728c3709864f2982472477c36ae7d7567
kiani14 <asusn43@yahoo.com>
2022-09-23 16:59:36 +0330

    ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿const contextMenu = document.getElementById("contextMenu");
+
+
+
+
+
+
+document.body.addEventListener('click', () => {
+    contextMenu.style.display = "none";
+})
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === "Escape")
+        contextMenu.style.display = "none";
+})
+
+
+document.body.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+    contextMenu.style.display = "block";
+    contextMenu.style.left = event.pageX;
+    contextMenu.style.top = event.pageY;
+
+})
